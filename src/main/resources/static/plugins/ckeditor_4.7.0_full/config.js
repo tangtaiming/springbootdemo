d8bfa67a48c4bba505b107d3444f90bb7f67747a
tangtaiming <accountnum@sina.cn>
2019-11-30 17:30:48 +0800

    ck

---
+++ src/main/resources/static/plugins/ckeditor_4.7.0_full/config.js
@@ -0,0 +1,34 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	config.toolbarGroups = [ {
+		name : 'others'
+	}, {
+		name : 'basicstyles',
+		groups : [ 'basicstyles', 'cleanup' ]
+	}, {
+		name : 'styles'
+	}, {
+		name : 'insert'
+	}, {
+		name : 'links'
+	}, {
+		name : 'colors'
+	}, {
+		name : 'paragraph',
+		groups : [ 'list', 'align' ]
+	}, {
+		name : 'document',
+		groups : [ 'mode', 'document', 'doctools' ]
+	}, {
+		name : 'tools'
+	} ];
+
+	config.removePlugins = 'horizontalrule,specialchar,pagebreak,iframe,smiley,flash,preview,blockquote,save,newpage,print,templates,showblocks';
+};
